4ef314ccbc68dc5a3e90dc99f906d68c0693cdf8
juergen-pecher <juergen.pecher@mydropteam.com>
2018-01-03 14:09:45 +0100

    index creation and doc indexing placed in different functions to set index more confortable

--- src/sentiment.py
+++ src/sentiment.py
@@ -59,36 +59,69 @@ class TweetStreamListener(StreamListener):
         now = datetime.datetime.now()
         index_suffix = now.strftime("-%Y-%m-%U")
 
+        # date-formated variable for weekly indexes in elasticsearch
+        # we need the format yyyy/MM/dd HH:mm:ss Z
+        created_temp = datetime.datetime.utcfromtimestamp(float(dict_data["timestamp_ms"]) / 1e3)
+        created = created_temp.strftime("%Y/%m/%d %H:%M:%S")
+
+        # index settings and mappings
+        createindex_body = {
+            'settings': {
+                'number_of_shards': 5,
+                'number_of_replicas': 1
+            },
+            'mappings': {
+                'tweet': {
+                    'date_detection': True,
+                    'properties': {
+                        'date_formated': { 'type': 'date', 'format': 'yyyy/MM/dd HH:mm:ss' },
+                    }
+                }
+            }
+        }
+
+        docindex_body = {"user": dict_data["user"],
+                         "date": dict_data["created_at"],
+                         "date_formated": created,
+                         "lang": dict_data["lang"],
+                         "message": dict_data["text"],
+                         "tweet_id": dict_data["id_str"],
+                         "timestamp": dict_data["timestamp_ms"],
+                         "tweet_date": dict_data["created_at"],
+                         "is_quote_status": dict_data["is_quote_status"],
+                         "in_reply_to_status_id": dict_data["in_reply_to_status_id"],
+                         "in_reply_to_screen_name": dict_data["in_reply_to_screen_name"],
+                         "favorite_count": dict_data["favorite_count"],
+                         "tweet_text": dict_data["text"],
+                         "retweeted": dict_data["retweeted"],
+                         "retweet_count": dict_data["retweet_count"],
+                         "is_quote_status": dict_data["is_quote_status"],
+                         "quote_count": dict_data["quote_count"],
+                         "reply_count": dict_data["reply_count"],
+                         "place": dict_data["place"],
+                         "coordinates": dict_data["coordinates"],
+                         "entities": dict_data["entities"],
+                         "polarity": tweet.sentiment.polarity,
+                         "subjectivity": tweet.sentiment.subjectivity,
+                         "sentiment": sentiment
+                         }
+
         # add text and sentiment info to elasticsearch
         # see for available values:
         # https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/tweet-object
-        es.index(index=elastic_index + index_suffix,
-                 doc_type="tweet",
-                 body={"author": dict_data["user"]["screen_name"],
-                       "author_id": dict_data["user"]["id_str"],
-                       "author_image": dict_data["user"]["profile_image_url_https"],
-                       "date": dict_data["created_at"],
-                       "lang": dict_data["lang"],
-                       "message": dict_data["text"],
-                       "tweet_id": dict_data["id_str"],
-                       "tweet_timestamp_ms": dict_data["timestamp_ms"],
-                       "tweet_date": dict_data["created_at"],
-                       "is_quote_status": dict_data["is_quote_status"],
-                       "in_reply_to_status_id": dict_data["in_reply_to_status_id"],
-                       "in_reply_to_screen_name": dict_data["in_reply_to_screen_name"],
-                       "favorite_count": dict_data["favorite_count"],
-                       "tweet_text": dict_data["text"],
-                       "retweeted": dict_data["retweeted"],
-                       "retweet_count": dict_data["retweet_count"],
-                       "is_quote_status": dict_data["is_quote_status"],
-                       "quote_count": dict_data["quote_count"],
-                       "reply_count": dict_data["reply_count"],
-                       "place": dict_data["place"],
-                       "coordinates": dict_data["coordinates"],
-                       "entities": dict_data["entities"],
-                       "polarity": tweet.sentiment.polarity,
-                       "subjectivity": tweet.sentiment.subjectivity,
-                       "sentiment": sentiment})
+
+        if es.indices.exists(elastic_index + index_suffix):
+            es.index(index=elastic_index + index_suffix,
+                           doc_type="tweet",
+                           id=dict_data["id_str"],
+                           body=docindex_body
+                           )
+
+        else:
+            es.indices.create(index=elastic_index + index_suffix,
+                              body = createindex_body
+                              )
+
         return True
 
     # on failure
